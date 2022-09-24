10b8832e30586e2dc3fd6e5c4fe52733b85fcd53
michgz <mdr.nz11@gmail.com>
2022-09-24 11:35:05 +0100

    Do with make

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ PYTHON = python
 all: test_verovio samuel-12.sfd
 
 otf: samuel-12.sfd src/build_otf.py _build
-	$(PYTHON) src/build_otf.py
+	$(PYTHON) src/build_otf.py samuel-12.sfd samuel-14.otf
 
 samuel-12.sfd: src/build_font.py
 	$(PYTHON) src/build_font.py samuel-11.sfdir samuel-12.sfd
@@ -12,5 +12,7 @@ samuel-12.sfd: src/build_font.py
 test_verovio: src/test_verovio.py samuel-12.sfd
 	$(PYTHON) src/test_verovio.py samuel-12.sfd
 
+_build:
+	mkdir _build
 
 .PHONY: test_verovio otf
--- src/build_otf.py
+++ src/build_otf.py
@@ -2,23 +2,35 @@
 
 import pathlib
 import subprocess
+import sys
+import textwrap
 
-# Delete any old file
-pathlib.Path("..", "samuel-14.otf").unlink()
+def build_otf(in_sfd, out_otf):
+
+    # Delete any old file
+    try:
+        out_otf.unlink()
+    except FileNotFoundError:
+        pass
 
 
-S = """
-import fontforge
+    S = """
+    import fontforge
 
-F = fontforge.open("../samuel-12.sfd")
+    F = fontforge.open("{0}")
 
-F.generate("../samuel-14.otf")
+    F.generate("{1}")
 
-"""
+    """.format(in_sfd.resolve(), out_otf.resolve())
 
-with open("s8.py", "w") as f8:
-    f8.write(S)
-    
-subprocess.run(['fontforge', '--script', 's8.py'])
+    with open("s8.py", "w") as f8:
+        f8.write(textwrap.dedent(S))
+        
+    subprocess.run(['fontforge', '--script', 's8.py'])
 
+
+if __name__=="__main__":
+    if len(sys.argv) < 3:
+        sys.exit(-1)
+    build_otf(pathlib.Path(sys.argv[1]), pathlib.Path(sys.argv[2]))
 
