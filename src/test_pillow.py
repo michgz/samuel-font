7806788edcb798093f81d58578679a956902c64c
michgz <mdr.nz11@gmail.com>
2022-10-15 12:46:58 +0100

    Space dynamics a bit better in the test image

--- src/test_pillow.py
+++ src/test_pillow.py
@@ -47,21 +47,23 @@ def test_pillow(in_otf):
         for I, UU in enumerate([0xE520, 0xE522, 0xE521]):
             DD.text((50 + 90*I, 40), struct.pack("<H", UU).decode('UTF-16LE'), font=font)
         
+        SPACINGS = [0, 40, 100, 180, 280, 380, 480, 580]
+        
         font = ImageFont.truetype(str(in_otf), size=64)
         PIANISSIMO = [(1, 0xE520), (2, 0xE52B), (3, 0xE52A), (4, 0xE529), (5, 0xE528), (6, 0xE527)]
         for I, UU in enumerate(PIANISSIMO):
-            DD.text((30 + 40*I, 240), u'\uE520' * UU[0], font=font)
-            DD.text((30 + 40*I, 340), struct.pack("<H", UU[1]).decode('UTF-16LE'), font=font)
+            DD.text((30 + SPACINGS[I], 240), u'\uE520' * UU[0], font=font)
+            DD.text((30 + SPACINGS[I], 340), struct.pack("<H", UU[1]).decode('UTF-16LE'), font=font)
 
         FORTISSIMO = [(1, 0xE522), (2, 0xE52F), (3, 0xE530), (4, 0xE531), (5, 0xE532), (6, 0xE533)]
         for I, UU in enumerate(FORTISSIMO):
-            DD.text((30 + 60*I, 440), u'\uE522' * UU[0], font=font)
-            DD.text((30 + 60*I, 540), struct.pack("<H", UU[1]).decode('UTF-16LE'), font=font)
+            DD.text((30 + SPACINGS[I], 440), u'\uE522' * UU[0], font=font)
+            DD.text((30 + SPACINGS[I], 540), struct.pack("<H", UU[1]).decode('UTF-16LE'), font=font)
             
         MIXED = [(u'\uE522\uE520', 0xE534), (u'\uE520\uE522', 0xE52E)]
         for I, UU in enumerate(MIXED):
-            DD.text((30 + 40*I, 640), UU[0], font=font)
-            DD.text((30 + 40*I, 740), struct.pack("<H", UU[1]).decode('UTF-16LE'), font=font)
+            DD.text((30 + 60*I, 640), UU[0], font=font)
+            DD.text((30 + 60*I, 740), struct.pack("<H", UU[1]).decode('UTF-16LE'), font=font)
 
             
         II.show()
